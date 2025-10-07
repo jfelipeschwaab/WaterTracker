1f38db78458b27e45849f8ccd63e7af27e2f68da
jfelipeschwaab <joaofsp.games@gmail.com>
2025-10-07 17:22:35 -0300

    Initial Commit

---
+++ Aqua/AquaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaApp.swift
+//  Aqua
+//
+//  Created by João Felipe Schwaab on 07/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AquaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
